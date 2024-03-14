a257c117b982b400517a32d1bb7a073af8fd825e
Wilman Paucar <wilpau15@gmail.com>
2024-03-14 11:50:50 -0500

    colocando la sentencia while para su repetion del codigo

--- lectura-entrada.py
+++ lectura-entrada.py
@@ -4,3 +4,9 @@ import machine
 import utime
 
 led_onboard = machine.Pin("LED", machine.Pin.OUT)
+
+while True:
+    led_onboard.value(1)
+    utime.sleep(10.5)
+    led_onboard.value(0)
+    utime.sleep(0.5)
